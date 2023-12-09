85e9c51fb6a85f070aa8d26ec8b05b46e5da38c6
flavien <flavien@gensdeconfiance.com>
2023-12-09 17:43:17 +0100

    AoC-9

---
+++ 09/flavien/AoC/index-2.mjs
@@ -0,0 +1,30 @@
+console.time()
+
+console.log(
+  (await Bun.file("./input.txt").text())
+    .split("\n")
+    .map((line) => line.split(" ").map((n) => Number(n)))
+    .map((sequence) => {
+      const sequencesAndSubsequences = [sequence]
+
+      while (!sequencesAndSubsequences.at(-1).every((n) => n === 0)) {
+        sequencesAndSubsequences.push(
+          sequencesAndSubsequences
+            .at(-1)
+            .reduce((subsequence, number, index, array) => {
+              if (index === array.length - 1) return subsequence
+
+              return [...subsequence, array[index + 1] - number]
+            }, [])
+        )
+      }
+
+      return sequencesAndSubsequences.reduceRight(
+        (accumulator, sequence) => sequence.at(0) - accumulator,
+        0
+      )
+    })
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+)
+
+console.timeEnd()
